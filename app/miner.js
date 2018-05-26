8b10b2f3e4e3b4f58599008c4de627edfbb23fc2
K J Venkatesh <kjvenky@gmail.com>
2018-05-27 02:07:20 +0530

    Reward Mining and add mining block.

--- app/miner.js
+++ app/miner.js
@@ -8,11 +8,13 @@ class Miner {
 
   mine() {
     const validTransactions = this.transactionPool.validTransactions();
-    // include a reward for the miner
-    // create a block consisting of the valid transactions
-    // synchronize the chains in the peer-to-peer server
-    // clear the transaction pool
-    // broadcast to every miner to clear their transaction pools
+    validTransactions.push(
+      Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
+    );
+    const block = this.blockchain.addBlock(validTransactions);
+    this.p2pServer.syncChains();
+    this.transactionPool.clear();
+    this.p2pServer.broadcastClearTransactions();
   }
 }
 
